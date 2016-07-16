9f572825d4a87d5dfd5f285cf6c348a5f0ea7bb0
oxuyingqio <oxuyingqio@qq.com>
2016-07-16 17:33:34 +0800

    X

---
+++ src/main/webapp/web/script/login/dialog.js
@@ -0,0 +1,22 @@
+/**
+ * 创建登录对话框
+ */
+function createDialog() {
+
+	$("#login").dialog({
+		width : 500,
+		height : 400,
+		closable : false,
+		noheader : true,
+		draggable : false,
+		style : {
+			backgroundColor : "#E8EEEE"
+		},
+		content : "<div id='content'><h1>Log in</h1></div>"
+	});
+}
+
+function applyContent() {
+	
+	
+}
